bd3e54f2164cf4fd5f9b9aacd02fde82f79fe46e
mesifer <brosfandy@gmail.com>
2023-11-01 15:01:38 +0700

    update action menu

--- src/components/editor/TipTap.vue
+++ src/components/editor/TipTap.vue
@@ -103,8 +103,8 @@ const ydoc = new Y.Doc()
 const getRandomElement = list => list[Math.floor(Math.random() * list.length)]
 
 const provider = new HocuspocusProvider({
-    // url: 'wss://editorhocus.oriens.my.id/',
-    url: 'ws://localhost:1234/',
+    url: 'wss://editorhocus.oriens.my.id/',
+    // url: 'ws://localhost:1234/',
     name: 'example-document',
     document: ydoc,
 })
