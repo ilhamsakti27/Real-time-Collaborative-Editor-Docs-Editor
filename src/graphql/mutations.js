35cbac8c16ad283bbe6331b73e1c8663af2763f3
ilhamsakti27 <ilhammuhammadsakti27@gmail.com>
2024-08-23 15:03:51 +0700

    update

--- src/graphql/mutations.js
+++
@@ -1,7 +0,0 @@
-import login from './mutation/login'
-import addCustomer from './mutation/addCustomer'
-
-export {
-  login,
-  addCustomer,
-}
--- src/graphql/subscriptions.js
+++
@@ -1,5 +0,0 @@
-import customerSubscription from './subscription/customerSubscription'
-
-export {
-  customerSubscription,
-}
