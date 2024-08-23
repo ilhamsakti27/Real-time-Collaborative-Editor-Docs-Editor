88c430d87db912badafa31f7675d2a22fcd8858c
ilhamsakti27 <ilhammuhammadsakti27@gmail.com>
2024-08-23 14:34:56 +0700

    update

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import Vue from 'vue'
 import VueAnimXYZ from '@animxyz/vue'
 import '@animxyz/core'
 import Notifications from 'vue-notification'
-import { apolloProvider } from '@/vue-apollo'
-import store from './store'
+// import { apolloProvider } from '@/vue-apollo'
+// import store from './store'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import App from './App.vue'
@@ -16,8 +16,8 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+  // store,
   vuetify,
-  apolloProvider,
+  // apolloProvider,
   render: h => h(App),
 }).$mount('#app')
